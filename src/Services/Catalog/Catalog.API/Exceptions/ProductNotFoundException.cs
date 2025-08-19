603bc1973aaf9a72a23bc2355f387bfea709c929
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-08-19 03:27:39 -0300

    add Product not found exception

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException(Guid Id) : base("Product not found!")
+    {
+    }
+}
